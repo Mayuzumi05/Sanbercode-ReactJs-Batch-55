d76f5f32e1331801cbf47e344b9b986266eca383
Alan Novianto <77398526+Mayuzumi05@users.noreply.github.com>
2024-03-28 20:15:57 +0700

    tugas day 4

---
+++ 04-javascript-loop-array/tugas4.js
@@ -0,0 +1,49 @@
+//soal nomor 1
+for (i=0;i<10;i++) {
+    console.log(i)
+}
+
+//soal nomor 2
+for (i=0;i<10;i++) {
+    if(i%2 == 1) {
+        console.log(i)
+    }
+}
+
+//soal nomor 3
+for (i=0;i<10;i++) {
+    if(i%2 == 0) {
+        console.log(i)
+    }
+}
+
+//soal nomor 4
+let array1 = [1,2,3,4,5,6]
+console.log(array1[5])
+
+//soal nomor 5
+let array2 = [5,2,4,1,3,5]
+console.log(array2.sort())
+
+//soal nomor 6
+let array3 = ["selamat", "anda", "melakukan", "perulangan", "array", "dengan", "for"]
+for (i=0;i<7;i++) {
+    console.log(array3.shift())
+}
+
+//soal nomor 7
+let array4 = [1, 2, 3, 4, 5, 6,7, 8, 9, 10]
+for (i=0;i<10;i++) {
+    if(array4[i]%2 == 0) {
+        console.log(array4[i])   
+    }
+}
+
+//soal nomor 8
+let kalimat= ["saya", "sangat", "senang", "belajar", "javascript"]
+console.log(kalimat.join(" "))
+
+//soal nomor 9
+var sayuran=[]
+sayuran.push("Kangkung", "Bayam", "Buncis", "Kubis", "Timun", "Seledri","Tauge");
+console.log(sayuran);
