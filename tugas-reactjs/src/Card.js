4a93074091561f7a1e381f864f719bd59a262de5
Alan Novianto <77398526+Mayuzumi05@users.noreply.github.com>
2024-04-02 15:53:47 +0700

    Tugas 6 - React: Introduction

---
+++ tugas-reactjs/src/Card.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+export default function Card() {
+    return (
+      <div className="card">
+        <h1>Hi, Saya Alan</h1>
+        <hr></hr>
+        <p>Saya dari Batch 55 sudah berhasil menginstall React</p>
+      </div>
+    );
+}
