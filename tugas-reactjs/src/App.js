67aa0c1ebb387772c8a0c2382aaf09f03d9dcb4a
Alan Novianto <77398526+Mayuzumi05@users.noreply.github.com>
2024-04-15 23:04:53 +0700

    Tugas 12 - React : Router

--- tugas-reactjs/src/App.js
+++ tugas-reactjs/src/App.js
@@ -1,11 +1,14 @@
 // import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Tugas6 from './tugas6/tugas6.js';
 import Tugas7 from './tugas7/tugas7.js';
 import Tugas8 from './tugas8/tugas8.jsx';
 import Tugas9 from './tugas9/tugas9.jsx';
 import Tugas10 from './tugas10/tugas10.jsx';
 import Tugas11 from './tugas11/tugas11.jsx';
+import Navbar from './navbar.jsx';
 
 function App() {
   return (
@@ -26,12 +29,23 @@ function App() {
     //   </header>
     // </div>
     <>
-      <Tugas6 />
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Tugas6/>} />
+          <Route path='/tugas7' element={<Tugas7 name="Himawan Alan Novianto" batch="55" email="alannovianto11@gmail.com"/>}  />
+          <Route path='/tugas8' element={<Tugas8/>} />
+          <Route path='/tugas9' element={<Tugas9/>} />
+          <Route path='/tugas10' element={<Tugas10/>} />
+          <Route path='/tugas11' element={<Tugas11/>} />
+        </Routes>
+      </BrowserRouter>
+      {/* <Tugas6 />
       <Tugas7 name="Himawan Alan Novianto" batch="55" email="alannovianto11@gmail.com"/>
       <Tugas8 />
       <Tugas9 />
       <Tugas10 />
-      <Tugas11 />
+      <Tugas11 /> */}
     </>
   );
 }
